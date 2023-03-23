794e1e8ae6e178119efa8a5b71ed466056da0d11
ashishgangwar07 <ashishgangwar07@gmail.com>
2023-03-23 22:34:34 +0530

    Setup Startup class, default route

--- consoleApptoWebAPI/Startup.cs
+++ consoleApptoWebAPI/Startup.cs
@@ -1,6 +1,26 @@
-﻿namespace consoleApptoWebAPI
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace consoleApptoWebAPI
 {
     public class Startup
     {
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+        }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello from web api app");
+                });
+            });
+        }
     }
 }
