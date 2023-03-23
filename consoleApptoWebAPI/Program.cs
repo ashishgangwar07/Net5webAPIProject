18936f985cc49d24a95c47f78f84036c442dd475
ashishgangwar07 <ashishgangwar07@gmail.com>
2023-03-23 22:20:59 +0530

    Added default host builder and web host

--- consoleApptoWebAPI/Program.cs
+++ consoleApptoWebAPI/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using System;
 
 namespace consoleApptoWebAPI
 {
@@ -6,7 +8,13 @@ namespace consoleApptoWebAPI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CreateHostBuilder(args).Build().Run();
         }
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
     }
 }
