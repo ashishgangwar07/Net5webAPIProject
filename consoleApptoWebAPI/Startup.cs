3d090440b6763e9ab85290f2cf0c4e9556b98dbf
ashishgangwar07 <ashishgangwar07@gmail.com>
2023-03-23 22:38:41 +0530

    Added AddControllers method

--- consoleApptoWebAPI/Startup.cs
+++ consoleApptoWebAPI/Startup.cs
@@ -9,17 +9,15 @@ namespace consoleApptoWebAPI
     {
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddControllers();
         }
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseRouting();
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello from web api app");
-                });
+                endpoints.MapControllers();
+               
             });
         }
     }
