d02fb3287fc21eae4866da075a4e950a4ca22b2c
ashishgangwar07 <ashishgangwar07@gmail.com>
2023-03-24 08:46:30 +0530

    Run, Use, Next, Map method in middleware

--- consoleApptoWebAPI/Startup.cs
+++ consoleApptoWebAPI/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
 
 namespace consoleApptoWebAPI
 {
@@ -14,6 +15,19 @@ namespace consoleApptoWebAPI
         }
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.Use(async (context, next) =>
+            {
+                await context.Response.WriteAsync("Hello from Use1 middleware \n");
+                await next();
+                await context.Response.WriteAsync("Hello from Use2 middleware \n");
+            });
+            app.Map("/ashish", customMessage);
+            app.Run(async context =>
+            {
+                await context.Response.WriteAsync("Hello from Run1 middleware \n");
+            });
+
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -22,7 +36,15 @@ namespace consoleApptoWebAPI
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
-               
+
+            });
+        }
+
+        private void customMessage(IApplicationBuilder app)
+        {
+            app.Use(async (context, next) =>
+            {
+                await context.Response.WriteAsync("Hello from custom Message \n");
             });
         }
     }
